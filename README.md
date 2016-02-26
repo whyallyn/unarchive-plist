559d98d76c29098ea1122697425adbcd50ef68e5
Allyn Stott <allynstott@gmail.com>
2016-02-26 04:32:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,118 @@
+Unarchive Plist
+===============
+This tool converts a property list (plist) file archived in the NSKeyedArchiver format to a serialized key-value paired plist for easier analysis.
+
+About
+-----
+Property list (plist) files are a key source of forensic information on OS X and iOS systems. When viewing plist files that have been archived in the NSKeyedArchiver format, it's [extremely difficult](http://www.mac4n6.com/blog/2016/1/1/manual-analysis-of-nskeyedarchiver-formatted-plist-files-a-review-of-the-new-os-x-1011-recent-items) to quickly gain context around the values stored in the plist. `unarchive_plist.py` was created to better facilitate investigation of these archived files by allowing for quick conversion to a serialized plist file with basic key-value pairs. Much easier for your viewing pleasure!
+
+**Before:**
+
+```
+"$objects" => [
+  0 => "$null"
+  1 => {
+    "NS.keys" => [
+      0 => <CFKeyedArchiverUID 0x7f807840ed70 [0x7fff72f38440]>{value = 2}
+      1 => <CFKeyedArchiverUID 0x7f807840ed90 [0x7fff72f38440]>{value = 3}
+      2 => <CFKeyedArchiverUID 0x7f807840edb0 [0x7fff72f38440]>{value = 4}
+    ]
+    "NS.objects" => [
+      0 => <CFKeyedArchiverUID 0x7f807840ee20 [0x7fff72f38440]>{value = 5}
+      1 => <CFKeyedArchiverUID 0x7f807840ee40 [0x7fff72f38440]>{value = 6}
+      2 => <CFKeyedArchiverUID 0x7f807840ee60 [0x7fff72f38440]>{value = 10}
+    ]
+    "$class" => <CFKeyedArchiverUID 0x7f807840eed0 [0x7fff72f38440]>{value = 9}
+  }
+  2 => "version"
+  3 => "properties"
+  4 => "items"
+  5 => 1
+  6 => {
+    "NS.keys" => [
+      0 => <CFKeyedArchiverUID 0x7f807840ef70 [0x7fff72f38440]>{value = 7}
+    ]
+    "NS.objects" => [
+      0 => <CFKeyedArchiverUID 0x7f807840efd0 [0x7fff72f38440]>{value = 8}
+    ]
+    "$class" => <CFKeyedArchiverUID 0x7f807840eed0 [0x7fff72f38440]>{value = 9}
+  }
+  7 => "com.apple.LSSharedFileList.MaxAmount"
+  8 => 10
+  9 => {
+    "$classname" => "NSDictionary"
+    "$classes" => [
+      0 => "NSDictionary"
+      1 => "NSObject"
+    ]
+  }
+  <snip>
+```
+
+**After:**
+
+```
+"items" => [
+    0 => {
+      "bookmark" => <626f6f6b d4020000 ...>
+      "properties" => {
+      }
+      "uniqueIdentifier" => "73F29D76-0446-4322-9A03-0C4F8557AE6C"
+      "name" => "Calculator.app"
+    }
+    1 => {
+      "bookmark" => <626f6f6b cc020000 ...>
+      "properties" => {
+      }
+      "uniqueIdentifier" => "98EFAD5E-0BDE-4A25-A735-8C39E9600A82"
+      "name" => "Safari.app"
+    }
+    2 => {
+      "bookmark" => <626f6f6b 00040000 ...>
+      "properties" => {
+      }
+      "uniqueIdentifier" => "1B3ECD35-1575-41A7-B797-9843C11520DD"
+      "name" => "Signal Private Messenger.app"
+    }
+    3 => {
+      "bookmark" => <626f6f6b d0020000 ...>
+      "properties" => {
+      }
+      "uniqueIdentifier" => "782F5EA9-8934-4FFC-88A2-9F37DB0867C5"
+      "name" => "Messages.app"
+    }
+    <snip>
+```
+
+Setup
+-----
+`unarchive_plist.py` must be run on an OS X system with Xcode installed as it utilizes PyObjC (the bridge between Python and Objective-C) for Apple specific classes and objects.
+
+Usage
+-----
+```
+$ python unarchive_plist.py -h
+usage: unarchive_plist.py [-h] -r PLIST_IN [-f {xml1,binary1}] -w PLIST_OUT
+
+Convert a keyed archived plist to a serialization with key-value pairs.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -r PLIST_IN, --read PLIST_IN
+                        Property List (plist or sfl) file to convert.
+  -f {xml1,binary1}, --format {xml1,binary1}
+                        Output format of plist_out.
+  -w PLIST_OUT, --write PLIST_OUT
+                        Filename to write the new serialized plist.
+```
+
+Example
+-------
+Convert archived RecentApplications plist to serialized XML plist:
+
+```
+$ python unarchive_plist.py \
+    -r com.apple.LSSharedFileList.RecentApplications.sfl \
+    -f xml1 \
+    -w com.apple.LSSharedFileList.RecentApplications_readable.plist
+```
---
+++ unarchive_plist.py
@@ -0,0 +1,127 @@
+"""Convert a keyed archived plist to a serialization with key-value pairs."""
+
+import argparse
+import sys
+
+# pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
+from Foundation import *
+import objc
+
+
+def nsdict_to_plist(nsdict, plist_name, output_format):
+    """Write NSDictionary out to a plist file."""
+    # select plist output format
+    if output_format == "xml1":
+        fmt = NSPropertyListXMLFormat_v1_0
+    elif output_format == "binary1":
+        fmt = NSPropertyListBinaryFormat_v1_0
+
+    # serialize the NSDictionary
+    srl, err = (
+        NSPropertyListSerialization.dataWithPropertyList_format_options_error_(
+            nsdict, fmt, 0, None))
+
+    if srl:
+        # convert NSData object into correct encoding
+        if output_format == "xml1":
+            plist = NSString.alloc().initWithData_encoding_(
+                srl, NSUTF8StringEncoding)
+        elif output_format == "binary1":
+            plist = bytearray(NSData.alloc().initWithData_(srl))
+        try:
+            # write string data to file
+            with open(plist_name, "wb") as pfile:
+                pfile.write(plist)
+        except IOError as error:
+            print("[ERROR] Failed to write to %s: %s" %
+                  (error.filename, error.strerror))
+    else:
+        print("[ERROR] Failed to serialize NSData as %s: %s" %
+              (output_format.upper(), err))
+
+
+def handle_none(ns_obj):
+    """Handle NSObjects being None."""
+    if ns_obj is None:
+        # NSObjects should be an empty string in type is None
+        return ""
+    else:
+        return ns_obj
+
+
+def sfllistitem_to_nsdict(sfllistitem):
+    """Convert Objective-C Class SFLListItem to an NSDictionary."""
+    # SFLListItem format documented here:
+    # http://michaellynn.github.io/2015/10/24/apples-bookmarkdata-exposed/
+    return handle_none(NSDictionary.dictionaryWithDictionary_({
+        "name": handle_none(sfllistitem.name()),
+        "uniqueIdentifier": handle_none(
+            sfllistitem.uniqueIdentifier().UUIDString()),
+        "properties": handle_none(sfllistitem.properties()),
+        "bookmark": handle_none(sfllistitem.bookmark())
+    }))
+
+
+def nskeyedarchive_to_nsdict(plist_name):
+    """Convert a NSKeyedArchive to a serializable NSDictionary."""
+    try:
+        # unarchive plist into a nsdict
+        nsdict = NSKeyedUnarchiver.unarchiveObjectWithFile_(plist_name)
+    except objc.error as err:
+        print("[ERROR] %s" % err)
+        sys.exit(1)
+
+    if nsdict:
+        items = []
+        # read SFLListItem "items" in order and return as list of nsdicts
+        for item in sorted(nsdict["items"], key=lambda k: k.order()):
+            items.append(sfllistitem_to_nsdict(item))
+    else:
+        print("[ERROR] Failed to unarchive %s. Check input name." % plist_name)
+        sys.exit(1)
+
+    # copy nsdict into a mutable nsdict and update "items"
+    new_nsdict = NSMutableDictionary.alloc().init()
+    new_nsdict.addEntriesFromDictionary_(nsdict)
+    new_nsdict["items"] = items
+
+    return new_nsdict
+
+
+def parse_arguments():
+    """Argument Parser."""
+    parser = argparse.ArgumentParser(
+        description=__doc__)
+    parser.add_argument(
+        "-r",
+        "--read",
+        dest="plist_in",
+        required=True,
+        help="Property List (plist or sfl) file to convert.")
+    parser.add_argument(
+        "-f",
+        "--format",
+        dest="output_format",
+        required=False,
+        default="xml1",
+        choices=["xml1", "binary1"],
+        help="Output format of plist_out.")
+    parser.add_argument(
+        "-w",
+        "--write",
+        dest="plist_out",
+        required=True,
+        help="Filename to write the new serialized plist.")
+    return parser.parse_args()
+
+
+def main():
+    """Main function."""
+    args = parse_arguments()
+    new_nsdict = nskeyedarchive_to_nsdict(args.plist_in)
+    nsdict_to_plist(new_nsdict, args.plist_out, args.output_format)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
